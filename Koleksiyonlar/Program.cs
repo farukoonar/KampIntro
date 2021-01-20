fca6ef207d0937d0d623c9c7c802bba0dbef7578
Faruk Onar <farukoonar@gmail.com>
2021-01-20 23:50:33 +0300

    ders bitti

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler1 = new string[] { "Kerem", "Engin", "Murat", "Halil" };
+            //Console.WriteLine(isimler1[0]);
+            //Console.WriteLine(isimler1[1]);
+            //Console.WriteLine(isimler1[2]);
+            //Console.WriteLine(isimler1[3]);
+            //Console.WriteLine(isimler1[4]);
+
+            List<string> isimler2 = new List<string> { "Kerem", "Engin", "Murat", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            isimler2.Add("BERKAYYYYYYYY");
+            Console.WriteLine(isimler2[5]);
+        }
+    }
+}

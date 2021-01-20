d56a0adf98ab2d5506995a1fb6188487ab3e083d
Faruk Onar <farukoonar@gmail.com>
2021-01-20 22:30:31 +0300

    Kamp Deneme

---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 1;
+            product1.Name = "Masa";
+            product1.UnitPrice = 120.99;
+            product1.UnitsInStock = 67;
+
+            Product product2 = new Product { Id = 2, CategoryId = 2, Name = "Kazak", UnitPrice = 70.99, UnitsInStock = 54 };
+            
+            //case sensitive
+            //PascalCase - camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Update(product1);
+
+            //ref - out
+        }
+    }
+}

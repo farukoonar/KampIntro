392e696f414906428bbd892b2a439cb6517455c7
Faruk Onar <farukoonar@gmail.com>
2021-01-24 21:26:09 +0300

    4.gün

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+        //Metotlar Kullanılmak Zorunda
+        //hepsinde ortak özellikler var ama kodları farklıdır
+        void Hesapla();
+        void BirSeyYap();
+    }
+}
